6e05a2a76eab8379842a42e7de8dab25cd0e1ce9
EnnaLeuark <afrenz@posteo.de>
2022-03-19 09:12:48 +0100

    added Phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,23 @@
+import React from "react";
+import symbol from "./images/audio-symbol.png";
+import "./Phonetics.css";
+import ReactAudioPlayer from "react-audio-player";
+
+export default function Phonetics(props) {
+  console.log(props.phonetics);
+
+  return (
+    <div>
+      <p>
+        {/* <img src={symbol} alt="audioSymbol" /> */}
+        {props.phonetics.text}`
+        <br />
+        <ReactAudioPlayer
+          src="https://api.dictionaryapi.dev/media/pronunciations/en/sunset-us.mp3"
+          autoPlay
+          controls
+        />{" "}
+      </p>
+    </div>
+  );
+}
